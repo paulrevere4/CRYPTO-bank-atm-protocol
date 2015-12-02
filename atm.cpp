a65a782e0b09c71e27169b2bc82696206564f503
josephhitchcock <hitchj@rpi.edu>
2015-12-01 21:51:30 -0500

    Added socket functionality to allow ATM to connect to bank

--- atm.cpp
+++ atm.cpp
@@ -3,21 +3,65 @@
 #include <string>
 #include <map>
 #include <cstdlib>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	cout << "Hello world! I am an ATM!" << endl;
-
+	
 	if (argc != 2)
 	{
 		cout << "USAGE: ./atm.exe <connect-port>" << endl;
 		return 1;
 	}
+	
+	// Connecting to the bank
+	
+	int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-	int port = atoi(argv[1]);
+    char buffer[256];
+    
+    portno = atoi(argv[1]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (sockfd < 0)
+    {
+    	perror("ERROR opening socket");
+    }
+        
+    server = gethostbyname("localhost");
+    cout << server << endl;
+    
+    if (server == NULL)
+    {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    serv_addr.sin_port = htons(portno);
+    
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+        perror("ERROR connecting");
+    }
+    
+    // Now connected to the bank
+	
 
-	cout << "Going to connect on port " << port << endl;
 
 	string users [3] = {"Alice", "Bob", "Eve"};
 	map<string, string> pins;
@@ -57,6 +101,8 @@ int main(int argc, char** argv)
 
 		string command;
 		cin >> command;
+		
+		// These take place locally in the ATM
 
 		if (command == "Login")
 		{
@@ -82,12 +128,28 @@ int main(int argc, char** argv)
 				}
 			}
 		}
+		
+		else if (command == "Logout")
+		{
+			if (loggedIn)
+			{
+				cout << "Logging out " << username << endl;
+				loggedIn = false;
+				username = "";
+			}
+			else
+			{
+				cout << "ERROR: Not logged in" << endl;
+			}
+		}
 
 		else if (command == "Exit")
 		{
 			cout << "Thank you for visiting" << endl;
 			break;
 		}
+		
+		// These require messages to be sent to the bank
 
 		else
 		{
@@ -100,7 +162,31 @@ int main(int argc, char** argv)
 			{
 				if (command == "Balance")
 				{
-					cout << "Printing " << username << "'s balance" << endl;
+					bzero(buffer,256);
+					string msg = username + "Balance";
+					int i;
+					for (i = 0; i < msg.size(); i++)
+					{
+						buffer[i] = msg[i];
+					}
+					msg[i] = '\0';
+					n = write(sockfd,buffer,strlen(buffer));
+    
+    				if (n < 0) 
+    				{
+         				perror("ERROR writing to socket");
+    				}
+    
+    				bzero(buffer,256);
+    				n = read(sockfd,buffer,255);
+    
+    				if (n < 0)
+    				{
+         				perror("ERROR reading from socket");
+    				}
+    
+    				printf("%s\n",buffer);
+					//cout << "Printing " << username << "'s balance" << endl;
 				}
 
 				if (command == "Withdraw")
@@ -125,16 +211,11 @@ int main(int argc, char** argv)
 					}
 				}
 
-				if (command == "Logout")
-				{
-					cout << "Logging out " << username << endl;
-					loggedIn = false;
-					username = "";
-				}
+				
 			}
 		}
 		cout << endl;
 	}
-
+	close(sockfd);
 	return 0;
 }
