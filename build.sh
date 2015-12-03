bedddf6a5b45947277ad96e453fa11993f717e31
paulrevere4 <revereiv@gmail.com>
2015-12-03 16:00:15 -0500

    Updated build scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
+echo ""
 echo "Building..."
+echo "[+] Building atm:"
 g++ atm.cpp encrypt_decrypt.cpp -lcryptopp -o atm
+echo "[!] atm built!"
+echo ""
+echo "[+] Building bank:"
 g++ bank.cpp encrypt_decrypt.cpp -pthread -lcryptopp -o bank
+echo "[!] bank built!"
+echo ""
+echo "[+] Building proxy:"
 g++ proxy.cpp -o proxy
-
-echo "Done Building, run the following commands"
-echo "./atm <port1>"
-echo "./bank <port2>"
-echo "./proxy <port1> <port2>"
+echo "[!] proxy built!"
+echo ""
+echo "Done Building, run the following commands on separate terminals"
+echo "$ ./atm <port1>"
+echo "$ ./bank <port2>"
+echo "$ ./proxy <port1> <port2>"
+echo ""
--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+echo ""
 echo "Running Setup..."
+echo "adding dependency 'g++'"
+apt-get install g++
+echo "done!"
+echo ""
+echo "adding dependency 'libcrypto++-dev'"
 apt-get install libcrypto++-dev
-echo "Installed libcrypto"
+echo "libcrypto++-dev is installed"
+echo ""
+echo "Setup complete!"
+echo "Run '$ source build.sh' to compile"
+echo ""
