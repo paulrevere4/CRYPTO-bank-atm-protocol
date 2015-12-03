21ca01705af0447424df82ca0ec775b89687f98e
paulrevere4 <revereiv@gmail.com>
2015-12-02 21:05:14 -0500

    Hashing with encryption

--- encypt_decrypt.cpp
+++ encypt_decrypt.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <utility>
+#include <cassert>
 
 #include <crypto++/rsa.h>
 #include <crypto++/osrng.h>
@@ -34,15 +35,20 @@ getNewKeys()
 }
 
 string
-encrypt(RSA::PublicKey publicKey, string plaintext)
+hash_and_encrypt(RSA::PublicKey publicKey, string plaintext)
 {
   string retVal;
+
+  unsigned char hashed[21];
+  hashed[20] = '\0';
+  SHA().CalculateDigest(hashed, (byte *)plaintext.c_str(), plaintext.length());
+  string hash_and_plaintext = string((char *)hashed) + plaintext;
 
   AutoSeededRandomPool randomNumberGenerator;
 
   RSAES_OAEP_SHA_Encryptor e(publicKey);
   
-  StringSource ss1(plaintext, 
+  StringSource ss1(hash_and_plaintext, 
       true,
       new PK_EncryptorFilter(randomNumberGenerator, 
           e, 
@@ -71,26 +77,70 @@ decrypt(RSA::PrivateKey privateKey, string ciphertext)
   return retVal;
 }
 
+string
+get_message_wout_hash(string message_w_hash)
+{
+  string retVal = "";
+  for ( unsigned int i = 20; i < message_w_hash.length(); i++ )
+  {
+    retVal += message_w_hash[i];
+  }
+  return retVal;
+}
+
+bool
+verify_message(string message_w_hash)
+{
+  string message_wout_hash = get_message_wout_hash(message_w_hash);
+
+  unsigned char hashed[21];
+  hashed[20] = '\0';
+  SHA().CalculateDigest(hashed, (byte *)message_wout_hash.c_str(), message_wout_hash.length());
+  
+  return string((char *)hashed)+message_wout_hash == message_w_hash;
+}
+
 int main()
 {
+  
+
   //test function
   pair<RSA::PrivateKey, RSA::PublicKey> keys = getNewKeys();
 
   RSA::PrivateKey privateKey = keys.first;
   RSA::PublicKey publicKey = keys.second;
 
-  // string plain = "Alice.Transfer.Bob.XXXXXXXXXX";
-  string plain = "Bob.Balance";
+  string plain = "Alice.Transfer.Bob.XXXXXXXXXX";
+  // string plain = "Bob.Balance";
   cout << "plaintext: " << plain << endl;
 
+  unsigned char hashed[21];
+  hashed[20] = '\0';
+  cout << "SHA_digest_size: " << SHA::DIGESTSIZE << endl;
+  SHA().CalculateDigest(hashed, (byte *)plain.c_str(), plain.length());
+  
+  printf("hashed plaintext: %s\n", hashed);
+  string hash_and_plaintext = string((char *)hashed) + plain;
+  cout << "hash_and_plaintext: " << hash_and_plaintext << endl;
+
+
   //Encryption
-  string ciphertext = encrypt(publicKey, plain);
-  cout << "ciphertext: " << ciphertext << endl;
+  string ciphertext = hash_and_encrypt(publicKey, plain);
+  cout << "ciphertext: \n" << ciphertext << endl;
   cout << "ciphertext length: " << ciphertext.length() << endl;
 
   // Decryption
   string decryptedPlaintext = decrypt(privateKey, ciphertext);
   cout << "recovered text: " << decryptedPlaintext << endl;
+  assert(decryptedPlaintext == hash_and_plaintext);
+
+  //get the message without the hash
+  string message_wout_hash = get_message_wout_hash(decryptedPlaintext);
+  cout << "plaintext without hash: " << message_wout_hash << endl;
+  assert(message_wout_hash == plain);
+
+  assert(verify_message(decryptedPlaintext));
+  cout << "message verified" << endl;
 
   return 0;
 }
